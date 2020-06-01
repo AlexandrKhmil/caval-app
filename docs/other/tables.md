28f86d7f52a907b8c99d3cb359a996b250e20b3e
Alex <oleksandr.khmil2@gmail.com>
2020-06-01 12:02:00 +0300

    init

---
+++ docs/other/tables.md
@@ -0,0 +1,10 @@
+# TABLES
+
+## ACCOUNT
+
+| KEY | Field | Type |
+| --- | --- | --- |
+| pk | id | serial |
+|  | email | character varying |
+|  | password | character varying |
+|  | reg_at | timestamp with time zone |
---
+++ docs/sql/tables.sql
@@ -0,0 +1,7 @@
+create table account (
+  id serial not null,
+  email character varying not null,
+  password character varying not null,
+  reg_at timestamp with time zone not null default now(),
+  constraint pk_account_id primary key (id)
+);
